342716a962b1f1014bb6e82741005bde7a2709b5
sheng <fourteen8155@gmail.com>
2023-03-02 19:11:57 +0800

    feat: test js  layout

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+const contentText = document.querySelectorAll(".content-text");
+
+// contentText.forEach((option, optionIndex) => {
+//   option.addEventListener("click", () => {
+//     document.querySelector(".left-title-font").innerHTML = `/ 0${
+//       optionIndex + 1
+//     }`;
+
+// //     if (optionIndex === selectedIndex) {
+// //       option.classList.add("content-acctive");
+// //     } else {
+// //       option.classList.remove("content-acctive");
+// //     }
+// //   });
+// });
+
+contentText.forEach((item, index) => {
+  item.addEventListener("click", () => {});
+});
