1408a1166c27ba0e496dbc881a1d43bf1c2bd23a
Sheng <fourteen8155@gmail.com>
2023-03-03 13:39:43 +0800

    feat: finished right-content layout

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,19 @@
-const contentText = document.querySelectorAll(".content-text");
+// const contentText = document.querySelectorAll(".content-text");
 
-// contentText.forEach((option, optionIndex) => {
-//   option.addEventListener("click", () => {
-//     document.querySelector(".left-title-font").innerHTML = `/ 0${
-//       optionIndex + 1
-//     }`;
+// // contentText.forEach((option, optionIndex) => {
+// //   option.addEventListener("click", () => {
+// //     document.querySelector(".left-title-font").innerHTML = `/ 0${
+// //       optionIndex + 1
+// //     }`;
 
-// //     if (optionIndex === selectedIndex) {
-// //       option.classList.add("content-acctive");
-// //     } else {
-// //       option.classList.remove("content-acctive");
-// //     }
-// //   });
+// // //     if (optionIndex === selectedIndex) {
+// // //       option.classList.add("content-acctive");
+// // //     } else {
+// // //       option.classList.remove("content-acctive");
+// // //     }
+// // //   });
+// // });
+
+// contentText.forEach((item, index) => {
+//   item.addEventListener("click", () => {});
 // });
-
-contentText.forEach((item, index) => {
-  item.addEventListener("click", () => {});
-});
