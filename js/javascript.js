5528f56582032715511e3c7ee9530818eff83159
Sheng <fourteen8155@gmail.com>
2023-03-07 03:45:12 +0800

    feat:add line motion and nav avtive

--- js/javascript.js
+++ js/javascript.js
@@ -18,9 +18,12 @@
 //   item.addEventListener("click", () => {});
 // });
 const Btn = document.querySelectorAll(".content-text");
-Btn.forEach((item) => {
+const BtnNumber = document.querySelector(".left-title-font");
+
+Btn.forEach((item, index) => {
   item.addEventListener("click", () => {
-    Btn.forEach((item2) => item2.classList.remove("content-acctive"));
-    item.classList.add("content-acctive");
+    Btn.forEach((item2) => item2.classList.remove("content-active"));
+    item.classList.add("content-active");
+    BtnNumber.innerHTML = `/ 0${index + 1}`;
   });
 });
