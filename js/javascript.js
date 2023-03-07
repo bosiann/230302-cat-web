fd47d17777cfe0932721e826e70ffd0ca5d37c7f
Sheng <fourteen8155@gmail.com>
2023-03-07 16:42:28 +0800

    feat:deleted annotation add icon

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,3 @@
-// const contentText = document.querySelectorAll(".content-text");
-
-// // contentText.forEach((option, optionIndex) => {
-// //   option.addEventListener("click", () => {
-// //     document.querySelector(".left-title-font").innerHTML = `/ 0${
-// //       optionIndex + 1
-// //     }`;
-
-// // //     if (optionIndex === selectedIndex) {
-// // //       option.classList.add("content-acctive");
-// // //     } else {
-// // //       option.classList.remove("content-acctive");
-// // //     }
-// // //   });
-// // });
-
-// contentText.forEach((item, index) => {
-//   item.addEventListener("click", () => {});
-// });
 const Btn = document.querySelectorAll(".content-text");
 const BtnNumber = document.querySelector(".left-title-font");
 
